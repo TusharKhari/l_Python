53ae1f356be644aa08be94c795066eb187cefa31
Tushar Khari <tusharkhari@Tushars-MacBook-Air.local>
2025-06-10 01:38:17 +0200

    oops ps

---
+++ ch10-oops/ps/01.py
@@ -0,0 +1,15 @@
+
+class Programmer:
+    company="microsoft"
+    def __init__(self, name, salary, pin):
+        # pass
+        self.name=name
+        self.salary=salary
+        self.pin=pin
+        
+        
+        
+        
+p = Programmer("Khari", 123, 151201)
+print(p.name)
+        
