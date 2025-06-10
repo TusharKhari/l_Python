2454cfa25b14f61b485110c9f7e68b5565b12f2f
Tushar Khari <tusharkhari@Tushars-MacBook-Air.local>
2025-06-11 00:14:51 +0200

    adv py

---
+++ project_perfectguess/05_raising_exception.py
@@ -0,0 +1,12 @@
+
+a=int(input("Enter a number: "))
+b = int(input("Enter a second number"))
+
+if(b==0):
+    raise ZeroDivisionError("Hey our program is not meant to divide numbers by zero")
+else:
+    print(f"The division a/b is {a/b}")
+    
+
+
+
